b405f02a40906256abc488e1e0cfbb6ced87e926
hajar298 <hajarih@code.edu.az>
2023-08-14 11:21:17 +0400

    Add:Filter to index parfume

--- js/indexp.js
+++ js/indexp.js
@@ -1,9 +1,28 @@
-const category = document.querySelectorAll(".category");
-const cat = document.querySelectorAll(".cat");
-const sellests = document.querySelectorAll(".selles");
+document.addEventListener("DOMContentLoaded", function () {
+  const categoryButtons = document.querySelectorAll(".category");
+  const sellesItems = document.querySelectorAll(".selles");
 
-category.forEach((element) => {
-  element.addEventListener("click", (e) => {
-    element.classList.toggle("click");
+  let previousButton = null;
+
+  categoryButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      if (previousButton) {
+        previousButton.style.backgroundColor = "";
+      }
+
+      const filter = button.getAttribute("data-filter");
+      button.style.backgroundColor = "black";
+
+      sellesItems.forEach((item) => {
+        const category = item.querySelector(".cat").textContent.toLowerCase();
+        if (filter === "all" || category === filter) {
+          item.style.display = "block";
+        } else {
+          item.style.display = "none";
+        }
+      });
+
+      previousButton = button;
+    });
   });
 });
