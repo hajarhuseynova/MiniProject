10297810f5687cf394cef4fbe99f3e6659e2d142
hajar298 <hajarih@code.edu.az>
2023-08-16 14:00:42 +0400

    Add:AppUser table

---
+++ Back/Parfume.App/areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Parfume.App.Context;
+
+namespace Parfume.App.areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AccountController : Controller
+    {
+        private readonly ParfumeDbContext _context;
+        private readonly IWebHostEnvironment _environment;
+
+        public AccountController(ParfumeDbContext context, IWebHostEnvironment environment)
+        {
+            _context = context;
+            _environment = environment;
+        }
+
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+    }
+}
--- Back/Parfume.Data/Context/ParfumeDbContext.cs
+++ Back/Parfume.Data/Context/ParfumeDbContext.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Parfume.Core.Entities;
 
 namespace Parfume.App.Context
 {
-    public class ParfumeDbContext:DbContext
+    public class ParfumeDbContext: IdentityDbContext<AppUser>
     {
-      
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+          
+            base.OnModelCreating(modelBuilder);
+        }
         public ParfumeDbContext(DbContextOptions<ParfumeDbContext> options) : base(options)
         {
 
