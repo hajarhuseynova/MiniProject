314359b7917aa7d3745774bdb47166f5848bcabc
hajar298 <hajarih@code.edu.az>
2023-09-17 17:36:06 +0400

    Add:Parfume db

---
+++ Back/Parfume.App/bin/Release/net6.0/publish/parfumepublish/Parfume.App/wwwroot/assets/js/detailp.js
@@ -0,0 +1,28 @@
+const minus = document.getElementById("minus");
+const pilus = document.getElementById("pilus");
+const inputValue = document.getElementById("countInput");
+
+minus.addEventListener("click", () => {
+  if (inputValue.value > 1) {
+    inputValue.value = parseInt(inputValue.value) - 1;
+  }
+});
+
+pilus.addEventListener("click", () => {
+  inputValue.value = parseInt(inputValue.value) + 1;
+});
+
+function increaseLike(element) {
+  var likeCountElement = element.nextElementSibling;
+  var currentLikeCount = parseInt(likeCountElement.textContent);
+  likeCountElement.textContent = currentLikeCount + 1;
+}
+function decreaseDislike(element) {
+  var dislikeCountElement = element.nextElementSibling;
+  var currentDislikeCount = parseInt(dislikeCountElement.textContent);
+
+  if (currentDislikeCount > 0) {
+    dislikeCountElement.textContent = currentDislikeCount - 1;
+  }
+}
+
